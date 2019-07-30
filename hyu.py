7cd4603d71a7a4de8e72b2ae1f97733c83aede11
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-30 01:51:40 -0700

    Create hyu.py

---
+++ hyu.py
@@ -0,0 +1,26 @@
+X11=input()
+Y11=""
+Z11=""
+for i in X11:
+    if i=="x":
+        Z11="Y11"
+        Y11=Y11+Z11
+    elif i=="y":
+        Z11="b"
+        Y11=Y11+Z11
+    elif i=="z":
+        Z11="c"
+        Y11=Y11+Z11
+    elif i=="Y":
+        Z11="B"
+        Y11=Y11+Z11
+    elif i=="Z":
+        Z11="C"
+        Y11=Y11+Z11
+    elif i=="X":
+        Z11=="Y"
+    else:
+        Z11=ord(i)+3
+        Y11=Y11+str(chr(Z11))
+	
+print(a)
